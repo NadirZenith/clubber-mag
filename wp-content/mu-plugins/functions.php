a67dd176a6f1bfa0e1692a38b79b6725c506948e
NadirZenith <2cb.md2@gmail.com>
2014-03-30 18:14:50 +0200

    removed filter to hide admin bar

--- wp-content/mu-plugins/functions.php
+++ wp-content/mu-plugins/functions.php
@@ -4,10 +4,10 @@
 
 /*                ----------------------------              */
 /** hide notifications !! cant update */
-define('DISALLOW_FILE_MODS', true);
+/*define('DISALLOW_FILE_MODS', true);*/
 
 
-add_action('set_current_user', 'csstricks_hide_admin_bar');
+/*add_action('set_current_user', 'csstricks_hide_admin_bar');*/
 
 function csstricks_hide_admin_bar() {
       if (!current_user_can('edit_posts')) {
