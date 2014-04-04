3a35570ffdabcea115b5a087e9028061437680fd
NadirZenith <2cb.md2@gmail.com>
2014-04-04 21:24:45 +0200

    facebook code changed

--- wp-content/themes/clubber-magazine/facebook/like-box.php
+++ wp-content/themes/clubber-magazine/facebook/like-box.php
@@ -2,6 +2,6 @@
 
 <div class="bg-50 block-5 pb5 mt30">
 
-      <div class="fb-like-box" data-href="https://www.facebook.com/Clubber.Nightlife.Guide" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+      <div class="fb-like-box" data-href="https://www.facebook.com/Clubber.Mag" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
       
 </div>
