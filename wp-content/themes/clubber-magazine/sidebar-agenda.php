8250124af81328fa0ec4bd61fc4cdd990c9c5a9c
NadirZenith <2cb.md2@gmail.com>
2014-04-10 11:52:30 +0200

    added date url string to next prev fc buttons

--- wp-content/themes/clubber-magazine/sidebar-agenda.php
+++ wp-content/themes/clubber-magazine/sidebar-agenda.php
@@ -2,11 +2,25 @@
 
 
 <div class="mt30">&nbsp;</div>
+<div id="sticky">
 
-<div id="calendar" class="bg-50 block-5"></div>
+      <div id="calendar" class="bg-50 block-5"></div>
+      <?php
+      include_once 'banners/right-2-300-300.php';
+      ?>
+</div>
 
-<script type="text/javascript">
-      jQuery(document).ready(function($) {
+
+
+<?php
+/*    banners       */
+/*    FACEBOOK LIKE BOX       */
+include_once 'facebook/like-box.php';
+?>
+
+
+
+
 <?php
 $start_date = strtotime("now");
 if (isset($_GET['date'])) {
@@ -17,16 +31,20 @@ if (isset($_GET['date'])) {
       }
 }
 ?>
-            function updateQueryStringParameter(uri, key, value) {
-                  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-                  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-                  if (uri.match(re)) {
-                        return uri.replace(re, '$1' + key + "=" + value + '$2');
-                  }
-                  else {
-                        return uri + separator + key + "=" + value;
-                  }
+<script type="text/javascript">
+      function updateQueryStringParameter(uri, key, value) {
+            var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+            var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+            if (uri.match(re)) {
+                  return uri.replace(re, '$1' + key + "=" + value + '$2');
             }
+            else {
+                  return uri + separator + key + "=" + value;
+            }
+      }
+
+      jQuery(document).ready(function($) {
+
 
             var start_date = new Date('<?php echo date('r', $start_date) ?>');
 
@@ -36,14 +54,11 @@ if (isset($_GET['date'])) {
                         center: 'title',
                         right: 'prev,next'
                   },
-                  /*year: 2014,*/
-                  /*month: 2,*/
-                  /*date: 2,*/
                   firstDay: 1,
                   dayClick: function(date, allDay, jsEvent, view) {
                         var date_str = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
 
-                        var location = updateQueryStringParameter('<?php echo get_post_type_archive_link('event'); ?>', 'date', encodeURIComponent(date_str));
+                        var location = updateQueryStringParameter('<?php echo get_post_type_archive_link('agenda'); ?>', 'date', encodeURIComponent(date_str));
                         window.location = location;
 
                   },
@@ -61,19 +76,50 @@ if (isset($_GET['date'])) {
 
             $('#calendar').fullCalendar('gotoDate', start_date);
 
+            $('.fc-button-prev span').click(function() {
+                  var date = $('#calendar').fullCalendar('prev').fullCalendar('getDate');
+                  var date_str = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+                  var location = updateQueryStringParameter('<?php echo get_post_type_archive_link('agenda'); ?>', 'date', encodeURIComponent(date_str));
+                  window.location = location;
+
+                  /*alert('prev ' + date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear());*/
+                  return false;
+            });
+
+            $('.fc-button-next span').click(function() {
+                  var date = $('#calendar').fullCalendar('next').fullCalendar('getDate');
+
+                  var date_str = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+                  var location = updateQueryStringParameter('<?php echo get_post_type_archive_link('agenda'); ?>', 'date', encodeURIComponent(date_str));
+                  window.location = location;
+
+                  /*                  alert('next ' + date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear());*/
+                  return false;
+            });
+
+
+            /*     STICKY       */
+/* 
+            $(function() { // document ready
+
+                  var stickyTop = $('#sticky').offset().top; // returns number 
+                  console.log(stickyTop);
+                  $(window).scroll(function() { // scroll event  
+
+                        var windowTop = $(window).scrollTop(); // returns number
+
+                        if ((stickyTop + 500) < windowTop) {
+                              $('#sticky').css({position: 'fixed', top: 30});
+                        }
+                        else {
+                              $('#sticky').css('position', 'static');
+                        }
+
+                  });
+
+            });
       });
 
+ */
+
 </script>
-
-
-<?php
-/*    BANNER      */
-/* include_once 'banners/archive-event.php'; */
-/* include_once 'banners/right-2-300-300.php'; */
-
-/*    FACEBOOK LIKE BOX       */
-include_once 'banners/right-2-300-300.php';
-include_once 'facebook/like-box.php';
-
-/* include_once 'facebook/facepile.php'; */
-?>
