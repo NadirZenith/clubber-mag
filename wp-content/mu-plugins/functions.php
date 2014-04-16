cde7308e010c7c4ee8e78812fa8a143c39e228a0
NadirZenith <2cb.md2@gmail.com>
2014-04-16 11:40:54 +0200

    deleted unused code

--- wp-content/mu-plugins/functions.php
+++ wp-content/mu-plugins/functions.php
@@ -10,9 +10,9 @@
 /*add_action('set_current_user', 'csstricks_hide_admin_bar');*/
 
 function csstricks_hide_admin_bar() {
-      if (!current_user_can('edit_posts')) {
-            show_admin_bar(false);
-      }
+        if (!current_user_can('edit_posts')) {
+                show_admin_bar(false);
+        }
 }
 
 /*add_filter('show_admin_bar', '__return_false');*/
