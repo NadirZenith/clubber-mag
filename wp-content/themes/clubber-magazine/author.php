b450f9be95eb605c89075c2b124a68a2d320bb5e
NadirZenith <2cb.md2@gmail.com>
2014-05-10 06:19:57 +0200

    lots of changes

--- wp-content/themes/clubber-magazine/author.php
+++ wp-content/themes/clubber-magazine/author.php
@@ -13,9 +13,9 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
 $edit_perfil_url = add_query_arg(array('action' => 'edit'), get_author_posts_url($curauth->ID));
 //http://lab.dev/clubber-mag-dev/perfil/{current_author}/?action=edit
 
-/*$curusr = wp_get_current_user();*/
+/* $curusr = wp_get_current_user(); */
 
-/*if ($curusr && $curusr->data->user_login != 'clubber-mag') {wp_redirect(home_url()); }*/
+/* if ($curusr && $curusr->data->user_login != 'clubber-mag') {wp_redirect(home_url()); } */
 ?>
 
 
@@ -23,26 +23,18 @@ $edit_perfil_url = add_query_arg(array('action' => 'edit'), get_author_posts_url
 <?php get_header(); ?>
 
 <div id="container">
-
         <?php
         $action = get_query_var('action');
-        /* if ()  */
         switch ($action) {
                 case 'edit':
-
                         include (locate_template('templates/user-profile-edit.php'));
                         break;
 
                 default:
                         include (locate_template('templates/user-profile.php'));
-
-
                         break;
         }
-        /* require_once 'templates/user-profile.php'; */
         ?>
-
-
 
 </div>
 
