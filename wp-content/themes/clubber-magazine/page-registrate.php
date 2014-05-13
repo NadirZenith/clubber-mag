bd909d3ea3d9e8b9e3ea197f3cf717356849e58e
NadirZenith <2cb.md2@gmail.com>
2014-05-13 22:30:16 +0200

    corrected register template

---
+++ wp-content/themes/clubber-magazine/page-registrate.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template Name: Registrate Template
+ *
+ * Displays the registrate template.
+ *
+ */
+if (is_user_logged_in()) {
+        $current_user = wp_get_current_user();
+        wp_redirect(get_author_posts_url(get_current_user_id()));
+        exit;
+}
+?>
+<?php get_header(); ?>
+
+<div id="container">
+
+        <?php
+        $action = get_query_var('action');
+        //recover password
+        if ($action == 'lostpassword') {
+                ?>
+                <style>
+                        .user-forms .submit{
+                                margin-top: 30px;
+                        }
+                        .user-forms p
+                        {
+                                width: 50%;
+                        }
+                </style>
+
+                <div class="col-2-4 fl pb15 pt5 mt15 bg-50 block-5">
+                        <div class="ml5 pb15">
+                                <h1 class="">Recupera tu password</h1>
+                        </div>
+                        <div class="ml5">
+
+                                <?php
+                                echo do_shortcode('[wppb-recover-password]');
+                                ?>
+                        </div>
+                </div>
+                <?php
+                //login & register
+        } else {
+
+                if (!is_user_logged_in()) {
+                        include (locate_template('templates/user-profile-login.php'));
+                        ?>
+
+                        <?php
+                }
+        }
+        ?>
+
+</div><!-- #container -->
+
+<?php get_footer(); ?>
