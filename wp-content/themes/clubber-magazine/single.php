07e1c17b605fc6e381c69ae0b99dc8de7efb20be
NadirZenith <2cb.md2@gmail.com>
2014-06-06 16:34:31 +0200

    added support and style for gallery in news

--- wp-content/themes/clubber-magazine/single.php
+++ wp-content/themes/clubber-magazine/single.php
@@ -2,69 +2,77 @@
 <?php get_header(); ?>
 
 <div id="container">
-      <div id="primary">
-            <?php
-            /* d('single'); */
-            global $post;
-            if (have_posts()) {
-                  while (have_posts()) {
-                        the_post();
+        <div id="primary">
+                <?php
+                d('single');
+                global $post;
+                if (have_posts()) {
+                        while (have_posts()) {
+                                the_post();
+                                ?>
+
+                                <section class="bg-50 block-5" style="overflow:visible">
+                                        <article >
+                                                <header class="hover" style="top:0px; height: 50px;">
+                                                        <h1 class="ml5 sc-eee">
+                                                                <?php the_title(); ?>
+                                                        </h1>
+                                                </header>
+                                                <div class="featured-image" style="width:100%;">
+                                                        <?php
+                                                        /* the_post_thumbnail('single-thumb'); */
+                                                        the_post_thumbnail();
+                                                        ?>
+                                                </div>
+                                                <div class="fr mt5 mr5 cb">
+                                                        <span class=""style="color: #666;">
+                                                                <?php echo get_the_date(); ?>
+                                                        </span>
+                                                </div>
+                                                <div class="mt5 ml5 mr5 meddium cb">
+                                                        <?php
+                                                        the_content();
+                                                        ?>
+                                                </div>
+                                                <?php
+                                                include_once 'facebook/like-single.php';
+                                                ?>
+
+                                        </article>
+                                </section>
+                                <script type="text/javascript">
+                                        (function($) {
+                                                $('dt.gallery-icon a').fancybox();
+                                        })(jQuery);
+                                </script>
+                                <style>
+                                        .gallery img{
+                                                width: 95%;
+                                        }
+                                </style>
+                                <?php
+                                /* do_action('attitude_after_post'); */
+                        }
+                } else {
                         ?>
-
-                        <section class="bg-50 block-5" style="overflow:visible">
-                              <article >
-                                    <header class="hover" style="top:0px; height: 50px;">
-                                          <h1 class="ml5 sc-eee">
-                                                <?php the_title(); ?>
-                                          </h1>
-                                    </header>
-                                    <div class="featured-image" style="width:100%;">
-                                          <?php
-                                          /* the_post_thumbnail('single-thumb'); */
-                                          the_post_thumbnail();
-                                          ?>
-                                    </div>
-                                    <div class="fr mt5 mr5 cb">
-                                          <span class=""style="color: #666;">
-                                                <?php echo get_the_date(); ?>
-                                          </span>
-                                    </div>
-                                    <div class="mt5 ml5 mr5 meddium cb">
-                                          <?php
-                                          the_content();
-                                          ?>
-                                    </div>
-                                    <?php
-                                    include_once 'facebook/like-single.php';
-                                    ?>
-
-                              </article>
-                        </section>
+                        <h1 class="entry-title"><?php _e('No Posts Found.', 'attitude'); ?></h1>
                         <?php
-                        /* do_action('attitude_after_post'); */
-                  }
-            } else {
-                  ?>
-                  <h1 class="entry-title"><?php _e('No Posts Found.', 'attitude'); ?></h1>
-                  <?php
-            }
-            ?>
+                }
+                ?>
 
-            <div class="cb bg-50  block-5">
+                <div class="cb bg-50  block-5">
 
-                  <h1 class="ml5">Comentarios</h1>
-                  <?php
-                  include_once 'facebook/comments.php';
-                  ?>
-            </div>
+                        <h1 class="ml5">Comentarios</h1>
+                        <?php
+                        include_once 'facebook/comments.php';
+                        ?>
+                </div>
 
-      </div>
+        </div>
 
-      <div id="secondary" class="no-margin-left">
-            <?php get_sidebar('right'); ?>
-      </div><!-- #secondary -->
-
-
+        <div id="secondary" class="no-margin-left">
+                <?php get_sidebar('right'); ?>
+        </div><!-- #secondary -->
 
 </div><!--container-->
 
