711750ff8f7300da436c97cb726817a4c7e07249
NadirZenith <2cb.md2@gmail.com>
2014-06-25 14:15:02 +0200

    removed unused info

--- wp-content/themes/clubber-magazine/templates/user-profile/user-profile-info.php
+++ wp-content/themes/clubber-magazine/templates/user-profile/user-profile-info.php
@@ -49,7 +49,7 @@
 
                 </div>
                 <div class="meddium" style="text-align:justify; padding: 5px;">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        <!--Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. -->
                         <?php
                         /*echo get_the_author_meta('description');*/
                         ?>
