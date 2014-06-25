b7b7b6ef06c74f309adc4ede7dcdf34f75d0aff9
NadirZenith <2cb.md2@gmail.com>
2014-06-25 13:54:27 +0200

    removed added mu plugins!

--- wp-content/mu-plugins/functions.php
+++ wp-content/mu-plugins/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 require_once 'vendor/autoload.php';
 
  include_once 'pimple.php'; 
@@ -9,4 +9,5 @@ include_once 'nz-profile-functions.php';
 include_once 'nz-ajax-functions.php';
 include_once 'nz-url-functions.php';
 include_once 'debug/nz-debug-functions.php';
+ */
 
