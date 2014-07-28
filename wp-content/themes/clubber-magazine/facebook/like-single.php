a58292d7a549386dd8ff4e91692bb224f3592784
NadirZenith <2cb.md2@gmail.com>
2014-07-28 02:17:14 +0200

    added class

--- wp-content/themes/clubber-magazine/facebook/like-single.php
+++ wp-content/themes/clubber-magazine/facebook/like-single.php
@@ -1,6 +1,6 @@
 
 <?php ?>
 
-<div class="cb m15 p15">
+<div class="cb m15 p15" style="overflow: hidden">
       <div class="fb-like" data-href="<?php echo get_permalink() ?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 </div>
