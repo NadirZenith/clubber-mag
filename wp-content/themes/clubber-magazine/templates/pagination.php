3fdbd19bae8c8ebd71701fcf94f38e6d325fd6de
NadirZenith <2cb.md2@gmail.com>
2014-08-05 21:56:27 +0200

    changed text

--- wp-content/themes/clubber-magazine/templates/pagination.php
+++ wp-content/themes/clubber-magazine/templates/pagination.php
@@ -1,5 +1,5 @@
 
 <div class="pagination" >
-        <div class="previous fl"><?php previous_posts_link('previous - new') ?></div>
-        <div class="next fr"><?php next_posts_link('next - old') ?></div>
+        <div class="previous fl"><?php previous_posts_link('ANTERIOR') ?></div>
+        <div class="next fr"><?php next_posts_link('SIGUIENTE') ?></div>
 </div>
