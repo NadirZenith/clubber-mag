2acc9cd163efc92bf6071828c381f469db510fa6
NadirZenith <2cb.md2@gmail.com>
2014-08-07 21:34:56 +0200

    added featured

---
+++ wp-content/themes/clubber-magazine/templates/featured-cool-places.php
@@ -0,0 +1,87 @@
+
+
+<?php
+$args = array(
+      'post_type' => 'cool-place',
+      'posts_per_page' => 4,
+      'order' => 'rand',
+      'orderby' => 'meta_value_num',
+      'meta_query' => array(
+            array(
+                  'key' => 'featured',
+                  'value' => 1,
+                  'compare' => '=',
+            )
+      )
+);
+$query = new WP_Query($args);
+if ($query->have_posts()) {
+        ?>
+        <section class="featured-cool-places bg-50 block-5 mb30">
+                <h1>
+                        <span class="ml5">Cool Places recomendados</span>
+                </h1>
+
+                <div id="flexslider-featured-cool-places" class="">
+
+                        <ul class="slides">
+                                <?php
+                                while ($query->have_posts()) {
+                                        $query->the_post();
+                                        ?>
+                                        <li>
+                                                <article class = "fl col-1-4">
+                                                        <div class = "hover-2" style = "">
+                                                                <h2 class = "ml5" style = "line-height: normal">
+                                                                        <a style = "" href = "<?php the_permalink(); ?>">
+                                                                                <?php
+                                                                                echo get_the_title()
+                                                                                ?>
+                                                                        </a>
+                                                                </h2>
+                                                        </div>
+                                                        <a class="featured-image" href="<?php echo get_permalink($event->ID); ?>"  style="">
+                                                                <?php echo get_the_post_thumbnail(get_the_ID(), '290-160-thumb'); ?>
+                                                        </a>
+                                                </article>
+                                        </li>
+                                        <?php
+                                        $count +=1;
+                                } //END while
+                                ?>
+                        </ul>
+                </div>
+        </section>
+        <?php
+} //end if have posts
+?>
+
+
+<?php
+return;
+?>
+<script type="text/javascript">
+        jQuery(document).ready(function($) {
+
+                $('#flexslider-featured-cool-places').flexslider({
+                        animation: "slide",
+                        slideshowSpeed: 5000,
+                        controlNav: false,
+                        directionNav: false,
+                        pauseOnHover: false,
+                        direction: "horizontal",
+                        reverse: false,
+                        animationSpeed: 500,
+                        prevText: "<",
+                        nextText: ">",
+                        easing: "linear",
+                        slideshow: true,
+                        useCSS: false
+                });
+        });
+</script>
+
+<?php
+wp_reset_postdata();
+?>
+
