3099acff092fd544466f7a00c45017780d262f72
NadirZenith <2cb.md2@gmail.com>
2014-08-29 17:53:22 +0200

    seo term links

--- wp-content/themes/clubber-magazine/templates/featured-cool-places.php
+++ wp-content/themes/clubber-magazine/templates/featured-cool-places.php
@@ -14,45 +14,57 @@ $args = array(
             )
       )
 );
-$query = new WP_Query($args);
-if ($query->have_posts()) {
-        ?>
-        <section class="featured-cool-places bg-50 block-5 mb30">
-                <h1>
-                        <span class="ml5">Cool Places recomendados</span>
-                </h1>
+$query = new WP_Query( $args );
+if ( $query->have_posts() ) {
+    ?>
+    <section class="featured-cool-places bg-50 block-5 mb30">
+        <h1 class="ml5 fl">
+            Cool Places recomendados
+        </h1>
+        <p class="fl mt5 ml5">
+            (
+            <?php
+            $terms = get_terms( 'cool_place_type' );
+            foreach ( $terms as $term ) {
+                ?>
+                <a href="<?php echo get_term_link( $term ) ?>"><i><?php echo $term->name ?></i></a>
+                <?php
+            }
+            ?>
+            )
+        </p>
 
-                <div id="flexslider-featured-cool-places" class="">
+        <div id="flexslider-featured-cool-places" class="cb">
 
-                        <ul class="slides">
-                                <?php
-                                while ($query->have_posts()) {
-                                        $query->the_post();
-                                        ?>
-                                        <li>
-                                                <article class = "fl col-1-4">
-                                                        <div class = "hover-2" style = "">
-                                                                <h2 class = "ml5" style = "line-height: normal">
-                                                                        <a style = "" href = "<?php the_permalink(); ?>">
-                                                                                <?php
-                                                                                echo get_the_title()
-                                                                                ?>
-                                                                        </a>
-                                                                </h2>
-                                                        </div>
-                                                        <a class="featured-image" href="<?php echo get_permalink($event->ID); ?>"  style="">
-                                                                <?php echo get_the_post_thumbnail(get_the_ID(), '290-160-thumb'); ?>
-                                                        </a>
-                                                </article>
-                                        </li>
+            <ul class="slides">
+                <?php
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    ?>
+                    <li>
+                        <article class = "fl col-1-4">
+                            <div class = "hover-2" style = "">
+                                <h2 class = "ml5" style = "line-height: normal">
+                                    <a style = "" href = "<?php the_permalink(); ?>">
                                         <?php
-                                        $count +=1;
-                                } //END while
-                                ?>
-                        </ul>
-                </div>
-        </section>
-        <?php
+                                        echo get_the_title()
+                                        ?>
+                                    </a>
+                                </h2>
+                            </div>
+                            <a class="featured-image" href="<?php echo get_permalink( $event->ID ); ?>"  style="">
+                                <?php echo get_the_post_thumbnail( get_the_ID(), '290-160-thumb' ); ?>
+                            </a>
+                        </article>
+                    </li>
+                    <?php
+                    $count +=1;
+                } //END while
+                ?>
+            </ul>
+        </div>
+    </section>
+    <?php
 } //end if have posts
 ?>
 
@@ -61,24 +73,24 @@ if ($query->have_posts()) {
 return;
 ?>
 <script type="text/javascript">
-        jQuery(document).ready(function($) {
+    jQuery(document).ready(function($) {
 
-                $('#flexslider-featured-cool-places').flexslider({
-                        animation: "slide",
-                        slideshowSpeed: 5000,
-                        controlNav: false,
-                        directionNav: false,
-                        pauseOnHover: false,
-                        direction: "horizontal",
-                        reverse: false,
-                        animationSpeed: 500,
-                        prevText: "<",
-                        nextText: ">",
-                        easing: "linear",
-                        slideshow: true,
-                        useCSS: false
-                });
+        $('#flexslider-featured-cool-places').flexslider({
+            animation: "slide",
+            slideshowSpeed: 5000,
+            controlNav: false,
+            directionNav: false,
+            pauseOnHover: false,
+            direction: "horizontal",
+            reverse: false,
+            animationSpeed: 500,
+            prevText: "<",
+            nextText: ">",
+            easing: "linear",
+            slideshow: true,
+            useCSS: false
         });
+    });
 </script>
 
 <?php
