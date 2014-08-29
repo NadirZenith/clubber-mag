bf9b40c5e857f9a7ab820a9c67291f4de9df6a3b
NadirZenith <2cb.md2@gmail.com>
2014-08-29 19:33:59 +0200

    added

---
+++ wp-content/themes/clubber-magazine/403.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays the 404 error page of the theme.
+ *
+ * @package Theme Horse
+ * @subpackage Attitude
+ * @since Attitude 1.0
+ */
+?>
+
+<div style="text-align: center">
+        <h1>No tienes permiso!</h1>
+
+</div>
