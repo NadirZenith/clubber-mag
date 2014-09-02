189224fe49a3e0522300fd7b12b946acf9f081b2
NadirZenith <2cb.md2@gmail.com>
2014-09-02 18:10:59 +0200

    standard form design

--- wp-content/themes/clubber-magazine/page-template-form.php
+++ wp-content/themes/clubber-magazine/page-template-form.php
@@ -9,30 +9,29 @@
 
 <?php
 global $post;
-if (have_posts()) {
-        while (have_posts()) {
-                the_post();
+if ( have_posts() ) {
+      while ( have_posts() ) {
+            the_post();
 
-                do_action('attitude_before_post');
-                ?>
-                <section class="bg-50 block-5 pb15" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <header class="">
-                                <h1 class="ml5 mt5 bigger">
-                                        <?php the_title(); ?>
-                                </h1>
+            do_action( 'attitude_before_post' );
+            ?>
+            <section class="bg-50 block-5 pb15" id="post-<?php the_ID(); ?>">
+                  <div class="col-3-4" style="margin:auto">
+                        <header>
+                              <h1 class="ml5 mt5 bigger">
+                                    <?php the_title(); ?>
+                              </h1>
                         </header>
-                        <div class="mt5 ml5 meddium mr5 cb pb5">
-                                <?php
-                                the_content()
-                                ?>
-                        </div>
-
-                </section>
-                <?php
-        }
+                        <?php
+                        the_content()
+                        ?>
+                  </div>
+            </section>
+            <?php
+      }
 } else {
-        ?>
-        <h1 class="entry-title"><?php _e('No Posts Found.', 'attitude'); ?></h1>
-        <?php
+      ?>
+      <h1 class="entry-title"><?php _e( 'No Posts Found.', 'attitude' ); ?></h1>
+      <?php
 }
 ?>
