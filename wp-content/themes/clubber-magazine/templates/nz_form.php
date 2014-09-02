483aae404221aca649ae9fcb1f39193c62336ae1
NadirZenith <2cb.md2@gmail.com>
2014-09-02 17:37:53 +0200

    base template form

---
+++ wp-content/themes/clubber-magazine/templates/nz_form.php
@@ -0,0 +1,8 @@
+<section class="bg-50 block-5 pb15">
+      <div class="col-3-4" style="margin: auto">
+            <?php
+            nzs_display_messages();
+            echo $nz[ $nz_form ];
+            ?>
+      </div>
+</section>
