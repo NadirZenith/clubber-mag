0cf7c9ff1698a4b1eb1946b78649a019aed90999
NadirZenith <2cb.md2@gmail.com>
2014-09-02 17:39:56 +0200

    removed old forms

--- wp-content/themes/clubber-magazine/templates/recursos/sello.php
+++
@@ -1,9 +0,0 @@
-
-<section class="bg-50 block-5 pb15">
-        <div class="col-3-4" style="margin: auto">
-                <?php
-                nzs_display_messages();
-                echo $nz['label_form'];
-                ?>
-        </div>
-</section>
