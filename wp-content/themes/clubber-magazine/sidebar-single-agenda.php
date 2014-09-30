c33816189701fdcb1d016d95b23030373cc9c768
NadirZenith <2cb.md2@gmail.com>
2014-09-30 17:48:21 +0200

    added fb like

--- wp-content/themes/clubber-magazine/sidebar-single-agenda.php
+++ wp-content/themes/clubber-magazine/sidebar-single-agenda.php
@@ -1,183 +1,151 @@
 
-
-<?php
-?>
-
 <?php
 /* $nonce = wp_create_nonce('relate_user_' . get_current_user_id() . '_to_event'); */
 ?>
 
-<style>
-        .nz-relate{
-                margin: 5%;
-                /*padding: 5px 5px 10px 5px;*/
-                position:relative;
-        }
-        .nz-relate a{
-                text-decoration: none;
-
-        }
-        .nz-relate a.nz-relate-btn{
-                text-align:center;
-                border-radius:3px;
-                background-color:#ccc;
-                display:block;
-        }
-        .nz-relate a.nz-relate-btn:hover{
-                background-color:#777;
-        }
-        .nz-relate a.nz-get-relation{
-                position:absolute;
-                right: 5px;
-                top:0;
-        }
-
-        .nz-relate .nzr-icon{
-                background: url(<?php echo get_template_directory_uri() . '/images/btn/clubber-mag-icons.png' ?>) no-repeat ;
-                width:30px;
-                height:30px;
-                overflow: hidden;
-                /*display:block;*/
-                float:left;
-                margin-top:5px;
-        }
-        .nz-relate .nzr-active .nzr-icon{
-                background-position: -30px;
-        }
-
-</style>
 <div class="bg-50 block-5 pb5 mt15 ">
-        <?php
-        $event = get_the_ID();
-        $NZRelation = New NZRelation('events_to_users', 'event_id', 'user_id');
-        /* $NZRelation->install_table(); */
-        $event_participants = $NZRelation->getRelationFrom($event);
-        $user_subscribed = $NZRelation->hasRelationFrom($event, get_current_user_id());
+      <div class="mt5" style="color:#333; text-align: center; font-size: 18px;font-family: 'Russo One', sans-serif;">
+            Zona de interacción  
+      </div>
+      <hr class="ml5 mr5">
+      <?php
+      $event = get_the_ID();
+      $NZRelation = New NZRelation( 'events_to_users', 'event_id', 'user_id' );
+      /* $NZRelation->install_table(); */
+      $event_participants = $NZRelation->getRelationFrom( $event );
+      $user_subscribed = $NZRelation->hasRelationFrom( $event, get_current_user_id() );
 
-        if ($user_subscribed) {
-                $class = 'nzr-active';
-                $name = 'Asistiré';
-        } else {
-                $class = '';
-                $name = 'Me apunto!';
-        }
+      if ( $user_subscribed ) {
+            $class = 'nzr-active';
+            $name = 'Asistiré';
+      } else {
+            $class = '';
+            $name = 'Me apunto!';
+      }
 
-        $total_participants = count($event_participants);
-        $total_style = ($total_participants == 0) ? 'style="visibility:hidden"' : '';
-        /* $view_participants_url = add_query_arg(array('action' => 'get_event_users', 'event' => get_the_ID()), admin_url('admin-ajax.php')); */
-        ?>
-        <!--        
-        -->
-        <div id="nz-relate-user-to-event" class="nz-relate big">
-                <a id="relate_user_to_event" class="nz-relate-btn <?php echo $class ?>" href="#participar" >
-                        <span class="nzr-icon"></span>
-                        <span class="nzr-text"><?php echo $name ?></span>
-                </a>
+      $total_participants = count( $event_participants );
+      $total_style = ($total_participants == 0) ? 'style="visibility:hidden"' : '';
+      /* $view_participants_url = add_query_arg(array('action' => 'get_event_users', 'event' => get_the_ID()), admin_url('admin-ajax.php')); */
+      ?>
+      <div id="nz-relate-user-to-event" class="nz-relate big">
+            <a id="relate_user_to_event" class="nz-relate-btn <?php echo $class ?>" href="#participar" >
+                  <span class="nzr-icon"></span>
+                  <span class="nzr-text"><?php echo $name ?></span>
+            </a>
 
-                <a id="get-event-users" class="fancybox.ajax nz-get-relation" href="#participantes" <?php echo $total_style ?>>
-                        (<span class="nzr-total"><?php echo $total_participants ?></span>)
-                </a>
+            <a id="get-event-users" class="fancybox.ajax nz-get-relation" href="#participantes" <?php echo $total_style ?>>
+                  (<span class="nzr-total"><?php echo $total_participants ?></span>)
+            </a>
 
-        </div>
+      </div>
 
-        <script type="text/javascript">
+      <div class="ml15 mt30" style="">
+            <?php
+            nz_fb_like();
+            ?>
+      </div>
 
-                jQuery(document).ready(function($) {
-                        $('#relate_user_to_event').on('click', nzr_process_relation);
-                        $("#get-event-users").on('click', nzr_process_get_relation);
+      <script type="text/javascript">
+
+            jQuery(document).ready(function($) {
+                  $('#relate_user_to_event').on('click', nzr_process_relation);
+                  $("#get-event-users").on('click', nzr_process_get_relation);
 
 
-                        function nzr_process_relation(e) {
-                                e.preventDefault();
-                                $btn = $(this);
-                                $relation = $('#get-event-users');
-                                $relation_count = $relation.find('.nzr-total');
-                                if (!$btn.hasClass('nzr-active')) {
-                                        //relate
-                                        $btn.find('.nzr-text').html('Asistiré');
-                                        $relation.css('visibility', 'visible');
-                                        $relation.find('.nzr-total').html(parseInt($relation_count.text()) + 1);
-                                        nzr_set_relation_from();
-                                } else {
-                                        //unrelate
-                                        $btn.find('.nzr-text').html('Me apunto!');
-                                        count = parseInt($relation_count.text());
-                                        if (count == 1) {
-                                                $relation.css('visibility', 'hidden');
-                                        }
-                                        $relation.find('.nzr-total').html(count - 1);
-                                        nzr_set_relation_from(false);
+                  function nzr_process_relation(e) {
+                        e.preventDefault();
+                        $btn = $(this);
+                        $relation = $('#get-event-users');
+                        $relation_count = $relation.find('.nzr-total');
+                        if (!$btn.hasClass('nzr-active')) {
+                              //relate
+                              $btn.find('.nzr-text').html('Asistiré');
+                              $relation.css('visibility', 'visible');
+                              $relation.find('.nzr-total').html(parseInt($relation_count.text()) + 1);
+                              nzr_set_relation_from();
+                        } else {
+                              //unrelate
+                              $btn.find('.nzr-text').html('Me apunto!');
+                              count = parseInt($relation_count.text());
+                              if (count == 1) {
+                                    $relation.css('visibility', 'hidden');
+                              }
+                              $relation.find('.nzr-total').html(count - 1);
+                              nzr_set_relation_from(false);
 
-                                }
-                                $btn.toggleClass('nzr-active');
                         }
+                        $btn.toggleClass('nzr-active');
+                  }
 
-                        function nzr_process_get_relation(e) {
-                                e.preventDefault();
-                                nzr_get_relation_from();
+                  function nzr_process_get_relation(e) {
+                        e.preventDefault();
+                        nzr_get_relation_from();
 
-                        }
-
-
-                        function nzr_ajax(data) {
-                                defaults = {
-                                        event: '<?php echo get_the_ID() ?>'
-                                };
-                                settings = $.extend({}, defaults, data);
-                                jQuery.ajax({
-                                        url: ajaxurl,
-                                        data: settings,
-                                        type: 'GET',
-                                        success: function(data, status, xhr) {
-                                                $.fancybox({
-                                                        'transitionIn': 'elastic',
-                                                        'transitionOut': 'elastic',
-                                                        'speedIn': 600,
-                                                        'speedOut': 200,
-                                                        'overlayShow': false,
-                                                        'width': '80%',
-                                                        'height': '80%',
-                                                        'autoSize': false,
-                                                        'content': data
-                                                });
-                                        },
-                                        error: function(xhr) {
-                                                $.fancybox({
-                                                        'content': xhr.responseText
-                                                });
-                                        },
-                                        complete: function(xhr, status) {
-                                        }
-
-                                });
-                        }
+                  }
 
 
-                        function nzr_get_relation_from() {
-                                data = {
-                                        action: 'get_event_users',
-                                };
+                  function nzr_ajax(data) {
+                        defaults = {
+                              event: '<?php echo get_the_ID() ?>'
+                        };
+                        settings = $.extend({}, defaults, data);
+                        jQuery.ajax({
+                              url: ajaxurl,
+                              data: settings,
+                              type: 'GET',
+                              success: function(data, status, xhr) {
+                                    $.fancybox({
+                                          'transitionIn': 'elastic',
+                                          'transitionOut': 'elastic',
+                                          'speedIn': 600,
+                                          'speedOut': 200,
+                                          'overlayShow': false,
+                                          'width': '80%',
+                                          'height': '80%',
+                                          'autoSize': false,
+                                          'content': data
+                                    });
+                              },
+                              error: function(xhr) {
+                                    $.fancybox({
+                                          'content': xhr.responseText
+                                    });
+                              },
+                              complete: function(xhr, status) {
+                              }
 
-                                nzr_ajax(data);
-                        }
+                        });
+                  }
 
-                        function nzr_set_relation_from(state) {
-                                state = state === false ? 'unrelate' : 'relate';
-                                data = {
-                                        action: 'relate_user_to_event',
-                                        arg: state
-                                };
-                                nzr_ajax(data);
-                        }
 
-                })
+                  function nzr_get_relation_from() {
+                        data = {
+                              action: 'get_event_users',
+                        };
 
-        </script>
+                        nzr_ajax(data);
+                  }
+
+                  function nzr_set_relation_from(state) {
+                        state = state === false ? 'unrelate' : 'relate';
+                        data = {
+                              action: 'relate_user_to_event',
+                              arg: state
+                        };
+                        nzr_ajax(data);
+                  }
+
+            })
+
+      </script>
 
 </div>
+<div class="bg-50 mt30 block-5 pb5">
+      <?php
+      nz_fb_like_box( 'https://www.facebook.com/Clubber.Mag' );
+      ?>
+</div>
 <?php
-include_once 'facebook/like-box.php';
-
 /*    banners       */
 include_once 'banners/right-2-300-300.php';
 ?>
