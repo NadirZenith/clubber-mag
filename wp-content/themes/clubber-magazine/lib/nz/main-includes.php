f190358fd2e67495a29e77eb2d0781e63809de00
NadirZenith <2cb.md2@gmail.com>
2014-09-30 17:48:46 +0200

    added fb like

---
+++ wp-content/themes/clubber-magazine/lib/nz/main-includes.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'pages/recursos.php'; // Page recursos specific queries
+include_once 'archives/author.php'; // Archive author specific queries
+include_once 'social/facebook-config.php'; // Archive author specific queries
