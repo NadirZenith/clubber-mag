da6bcaec41aebc042ce98b8846a0832eee195719
NadirZenith <2cb.md2@gmail.com>
2014-10-01 00:45:31 +0200

    added tpl functions

--- wp-content/themes/clubber-magazine/lib/nz/main-includes.php
+++ wp-content/themes/clubber-magazine/lib/nz/main-includes.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once 'template.php'; // Page recursos specific queries
+/**/
+
 include_once 'pages/recursos.php'; // Page recursos specific queries
 include_once 'archives/author.php'; // Archive author specific queries
 include_once 'social/facebook-config.php'; // Archive author specific queries
