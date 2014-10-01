92e14d76af6e21674890223c22b02b346087ec7d
NadirZenith <2cb.md2@gmail.com>
2014-10-01 18:24:14 +0200

    added twitter

--- wp-content/themes/clubber-magazine/lib/nz/main-includes.php
+++ wp-content/themes/clubber-magazine/lib/nz/main-includes.php
@@ -6,3 +6,4 @@ include_once 'template.php'; // Page recursos specific queries
 include_once 'pages/recursos.php'; // Page recursos specific queries
 include_once 'archives/author.php'; // Archive author specific queries
 include_once 'social/facebook-config.php'; // Archive author specific queries
+include_once 'social/twitter-config.php'; // Archive author specific queries
