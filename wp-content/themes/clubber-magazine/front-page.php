403cec7bf70421bc6f5f2a799b90b1c10266108c
NadirZenith <2cb.md2@gmail.com>
2014-10-02 15:53:52 +0200

    removed ads

--- wp-content/themes/clubber-magazine/front-page.php
+++ wp-content/themes/clubber-magazine/front-page.php
@@ -1,62 +1,76 @@
 
 <ul class="home-list">
 
-        <!--  FEATURED POSTS -->
-        <li class="col-0">
-                <section class="featured-posts">
-                        <?php
-                        echo do_shortcode('[metaslider id=661]');
-                        ?>
-                </section>
-        </li>
+      <!--  FEATURED POSTS -->
+      <li class="col-0">
+            <section class="featured-posts">
+                  <?php
+                  echo do_shortcode( '[metaslider id=661]' );
+                  ?>
+            </section>
+      </li>
 
-        <!--  FEATURED EVENTS -->
-        <li class="col-0 ">
-                <section class="featured-events bg-50 block-5">
-                        <?php
-                        require_once 'library/structure/front/event.php';
-                        ?>
-                </section>
-        </li>
+      <!--  FEATURED EVENTS -->
+      <li class="col-0 ">
+            <section class="featured-events bg-50 block-5">
+                  <?php
+                  require_once 'library/structure/front/event.php';
+                  ?>
+            </section>
+      </li>
 
-        <!--  NEWS   -->
-        <li class="col-1 fl">
-                <section class="news bg-50 block-5" >
-                        <?php require_once 'library/structure/front/news.php'; ?>
-                </section>
+      <!--  NEWS   -->
+      <li class="col-1 fl">
+            <section class="news bg-50 block-5" >
+                  <?php require_once 'library/structure/front/news.php'; ?>
+            </section>
+            <?php
+            /*
+              <div class="banner-side featured-image" >
+              <?php echo do_shortcode('[sam id=2]'); ?>
+              </div>
+             */
+            ?>
+      </li>
+      <!--  MUSIC   -->
+      <li class="col-2 fr" style="">
+            <section class="music bg-50 block-5">
+                  <?php require_once 'library/structure/front/music.php'; ?>
+            </section>
+      </li>
+      <!--  PHOTO & VIDEO  -->
+      <li class="col-3 fl">
 
-                <div class="banner-side featured-image" >
-                        <?php
-                        echo do_shortcode('[sam id=2]');
-                        ?>
-                </div>
-        </li>
-        <!--  MUSIC   -->
-        <li class="col-2 fr" style="">
-                <section class="music bg-50 block-5">
-                        <?php require_once 'library/structure/front/music.php'; ?>
-                </section>
+            <section class="photo bg-50 block-5 pb15">
+                  <?php
+                  require_once 'library/structure/front/photo.php';
+                  ?>
+            </section>
 
-                <div class="banner-side featured-image">
-                        <?php
-                        echo do_shortcode('[sam id=3]');
-                        ?>
-                </div>
-        </li>
-        <!--  PHOTO & VIDEO  -->
-        <li class="col-3 fl">
-
-                <section class="photo bg-50 block-5">
-                        <?php
-                        require_once 'library/structure/front/photo.php';
-                        ?>
-                </section>
-
-                <section class="video bg-50 block-5">
-                        <?php
-                        require_once 'library/structure/front/video.php';
-                        ?>
-                </section>
-        </li>
+            <section class="video bg-50 block-5">
+                  <?php
+                  require_once 'library/structure/front/video.php';
+                  ?>
+            </section>
+      </li>
 
 </ul>
+<?php 
+/*
+?>
+<style>
+      .banner-bottom{
+            width: 100%;
+            margin: 12px;
+      }
+      .banner-bottom img{
+            margin-left: 120px;
+      }
+</style>
+<div class="cb"></div>
+<div class="banner-bottom cb">
+      <?php
+      echo do_shortcode( '[sam id=5]' );
+      ?>
+</div>
+ */
