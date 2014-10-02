c6df948dc11a0daeceb69d7d5d6d4f97a49e4667
NadirZenith <2cb.md2@gmail.com>
2014-10-02 15:56:56 +0200

    ..

--- wp-content/themes/clubber-magazine/page-template-archive-news.php
+++ wp-content/themes/clubber-magazine/page-template-archive-news.php
@@ -8,89 +8,87 @@
 ?>
 
 <?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$paged = (get_query_var( 'paged' )) ? get_query_var( 'paged' ) : 1;
 $args = array(
       'post_type' => 'post',
       'paged' => $paged,
 );
-/* $the_query = new WP_Query($args); */
-global $wp_query;
-/* $wp_query_temp = $wp_query; */
-unset($wp_query);
-$wp_query = new WP_Query($args);
+$query = new WP_Query( $args );
 ?>
 <div id="primary">
-        <h1>
-                <?php
-                echo strtoupper(get_the_title());
-                ?>
-        </h1>
+      <h1>
+            <?php
+            echo strtoupper( get_the_title() );
+            ?>
+      </h1>
 
-        <ul class="archive-list">
-                <?php
-                /* global $post; */
+      <ul class="archive-list">
+            <?php
+            /* global $post; */
 
-                if ($wp_query->have_posts()) {
-                        while ($wp_query->have_posts()) {
-                                $wp_query->the_post();
-                                ?>
-                                <li>
-                                        <section class="bg-50 block-5 mb15">
-                                                <article>
-                                                        <header>
-                                                                <h1 class="mt5">
-                                                                        <a class="ml5" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                                                                </h1>
-
-                                                        </header>
-                                                        <hr class="pb5">
-                                                        <div class="fl ml5 col-2-4 " style="">
-                                                                <div class="meddium bold" style="text-align:justify">
-                                                                        <p><?php echo wp_trim_words(get_the_content(), 20); ?></p>
-                                                                </div>
-                                                                <p class="">
-                                                                        <a class="readmore" href="<?php the_permalink() ?>" title="<?php the_title() ?>"> <?php echo __('Read more', 'attitude') ?></a>
-                                                                </p>
-
-                                                                <div  style="color: #666;">
-                                                                        <?php echo get_the_date() ?>
-
-                                                                </div>
-                                                        </div>
-
-                                                        <div class="fr col-2-4 nm" >
-                                                                <?php
-                                                                if (has_post_thumbnail()) {
-                                                                        ?>
-                                                                        <a class="featured-image" href="<?php the_permalink() ?>"  style="">
-                                                                                <?php
-                                                                                the_post_thumbnail('430-190-thumb');
-                                                                                ?>
-                                                                        </a>
-                                                                        <?php
-                                                                }
-                                                                ?>
-                                                        </div>
-
-                                                </article>
-                                        </section>
-                                </li>
-
-                                <?php
-                        }
-                } else {
+            if ( $query->have_posts() ) {
+                  while ( $query->have_posts() ) {
+                        $query->the_post();
                         ?>
-                        <h1 class=""><?php _e('No Posts Found.', 'attitude'); ?></h1>
-                        <?php
-                }
-                ?>
-        </ul>
+                        <li>
+                              <section class="bg-50 block-5 mb15">
+                                    <article>
+                                          <header>
+                                                <h1 class="mt5">
+                                                      <a class="ml5" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                                                </h1>
 
-        <?php
-        include (locate_template('templates/pagination.php'));
-        ?>
+                                          </header>
+                                          <hr class="pb5">
+                                          <div class="fl ml5 col-2-4 " style="">
+                                                <div class="meddium bold" style="text-align:justify">
+                                                      <p><?php echo wp_trim_words( get_the_content(), 20 ); ?></p>
+                                                </div>
+                                                <p class="">
+                                                      <a class="readmore" href="<?php the_permalink() ?>" title="<?php the_title() ?>"> <?php echo __( 'Read more', 'attitude' ) ?></a>
+                                                </p>
+
+                                                <div  style="color: #666;">
+                                                      <?php echo get_the_date() ?>
+
+                                                </div>
+                                          </div>
+
+                                          <div class="fr col-2-4 nm" >
+                                                <?php
+                                                if ( has_post_thumbnail() ) {
+                                                      ?>
+                                                      <a class="featured-image" href="<?php the_permalink() ?>"  style="">
+                                                            <?php
+                                                            the_post_thumbnail( '430-190-thumb' );
+                                                            ?>
+                                                      </a>
+                                                      <?php
+                                                }
+                                                ?>
+                                          </div>
+
+                                    </article>
+                              </section>
+                        </li>
+
+                        <?php
+                  }
+            } else {
+                  ?>
+                  <h1 class=""><?php _e( 'No Posts Found.', 'attitude' ); ?></h1>
+                  <?php
+            }
+            ?>
+      </ul>
+
+      <?php
+      global $wp_query;
+      $wp_query = $query;
+      include (locate_template( 'templates/pagination.php' ));
+      ?>
 </div>
 
 <div id="secondary">
-        <?php get_sidebar('right'); ?>
+      <?php get_sidebar( 'right' ); ?>
 </div><!-- #secondary -->
