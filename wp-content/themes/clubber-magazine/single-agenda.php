45be8cdd5d65304a0e269da935ee4ee7c87d72f4
NadirZenith <2cb.md2@gmail.com>
2014-10-03 14:21:30 +0200

    added span to tags

--- wp-content/themes/clubber-magazine/single-agenda.php
+++ wp-content/themes/clubber-magazine/single-agenda.php
@@ -161,7 +161,7 @@
                               <ul>
                                     <?php
                                     foreach ( $tags as $tag ) {
-                                          echo '<li>' . $tag->name . '</li>';
+                                          echo '<li><span>' . $tag->name . '</span></li>';
                                           /*echo '<li><a href="' . get_term_link( $tag ) . '">' . $tag->name . '</a></li>';*/
                                     }
                                     ?>
