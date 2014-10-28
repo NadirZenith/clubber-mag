7f395967467d6e4c85e4b3a8bed3d7a233dcd350
NadirZenith <2cb.md2@gmail.com>
2014-10-28 15:23:04 +0100

    ..

--- wp-content/themes/clubber-magazine/banners/single-event-1.php
+++ wp-content/themes/clubber-magazine/banners/single-event-1.php
@@ -1,8 +1,7 @@
 <?php ?>
-<!--<div class="mt30">-->
 <div class="bg-50 block-5 mt30">
       <div class="featured-image" >
-            <?php echo do_shortcode('[sam id=4]') ?>
+            <?php echo do_shortcode('[sam id=6]') ?>
       </div>
 
 
--- wp-content/themes/clubber-magazine/banners/single-event-2.php
+++ wp-content/themes/clubber-magazine/banners/single-event-2.php
@@ -1,7 +1,7 @@
 <?php ?>
 <div class="bg-50 block-5 mt30">
       <div class="featured-image" >
-            <?php echo do_shortcode('[sam id=6]') ?>
+            <?php echo do_shortcode('[sam id=4]') ?>
       </div>
 
 
