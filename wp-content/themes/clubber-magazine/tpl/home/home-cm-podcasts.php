5023677756400f523933dfca32c0a817e3ee2287
NadirZenith <2cb.md2@gmail.com>
2015-03-19 20:04:51 +0100

    changed posts per page

--- wp-content/themes/clubber-magazine/tpl/home/home-cm-podcasts.php
+++ wp-content/themes/clubber-magazine/tpl/home/home-cm-podcasts.php
@@ -11,7 +11,7 @@
                         <?php
                         $args = array(
                               'post_type' => 'into-the-beat',
-                              'posts_per_page' => 1,
+                              'posts_per_page' => 3,
                               'meta_query' => array(
                                     array(
                                           'key' => '_thumbnail_id',
