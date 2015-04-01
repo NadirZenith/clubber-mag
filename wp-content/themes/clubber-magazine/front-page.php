dc73875f90490ad11def7311c295f938df35db5d
NadirZenith <2cb.md2@gmail.com>
2015-04-02 01:10:44 +0200

    ..

--- wp-content/themes/clubber-magazine/front-page.php
+++ wp-content/themes/clubber-magazine/front-page.php
@@ -1,49 +1,47 @@
 <ul class="home-list">
 
-      <li class="col-1">
-            <?php get_template_part( 'tpl/parts/featured-events' ); ?>
-      </li>
+    <li class="col-1">
+        <?php get_template_part('tpl/parts/featured-events'); ?>
+    </li>
 
-      <li class="col-1 col-md-1-2 col-small fl">
-            <?php get_template_part( 'tpl/home/home-last-news' ); ?>
-      </li>
+    <li class="col-1 col-md-1-2 col-small fl">
+        <?php get_template_part('tpl/home/home-news'); ?>
+    </li>
 
-      <li class="col-1 col-md-1-2 col-small fr">
-            <?php get_template_part( 'tpl/home/home-last-music' ); ?>
-      </li>
+    <li class="col-1 col-md-1-2 col-small fr">
+        <?php get_template_part('tpl/home/home-music'); ?>
+    </li>
 
-      <li class="col-1 col-big fl">
-            <?php get_template_part( 'tpl/home/home-last-photo' ); ?>
+    <li class="col-1 col-big fl">
+        <?php get_template_part('tpl/home/home-photo'); ?>
 
-            <?php get_template_part( 'tpl/home/home-last-video' ); ?>
+        <?php get_template_part('tpl/home/home-video'); ?>
 
-            <?php get_template_part( 'tpl/home/newsletter' ); ?>
-      </li>
+        <?php get_template_part('tpl/home/newsletter'); ?>
+    </li>
 
-      <li class="col-1">
-            <div class="featured-image banner-bottom"> 
-                  <?php echo do_shortcode( '[sam id=5]' ); ?>
-            </div>
-      </li>
+    <li class="col-1">
+        <div class="featured-image banner-bottom"> 
+            <?php echo do_shortcode('[sam id=5]'); ?>
+        </div>
+    </li>
 
-      <li class="col-1 col-md-1-2 fl">
-            <?php get_template_part( 'tpl/home/home-cm-podcasts' ); ?>
+    <li class="col-1 col-md-1-2 fl">
+        <?php get_template_part('tpl/home/home-into-the-beat'); ?>
 
-      </li>
-      <li class="col-1 col-md-1-2 fl">
-            <?php get_template_part( 'tpl/home/home-shared-podcasts' ); ?>
+    </li>
+    <li class="col-1 col-md-1-2 fl">
+        <?php get_template_part('tpl/home/home-open-frequency'); ?>
 
-      </li>
+    </li>
 </ul>
 
 <script type="text/javascript">
-      jQuery(document).ready(function($) {
-            $('.home-slider').flexslider({
-                  controlNav: false,
-                  directionNav: false,
-                  pauseOnHover: true,
-                  /*useCSS: false*/
-
-            });
-      });
+    jQuery(document).ready(function ($) {
+        $('.home-slider').flexslider({
+            controlNav: false,
+            directionNav: false,
+            pauseOnHover: true
+        });
+    });
 </script> 
