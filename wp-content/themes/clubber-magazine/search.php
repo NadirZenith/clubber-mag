c159192bf9534a5834b6daa00e7d88fde730f959
NadirZenith <2cb.md2@gmail.com>
2015-04-02 02:37:17 +0200

    ..

--- wp-content/themes/clubber-magazine/search.php
+++ wp-content/themes/clubber-magazine/search.php
@@ -1,112 +1,97 @@
 
 <?php
-$search = get_query_var( 's' );
-/* $search = 'clubber+mag'; */
+$search = get_query_var('s');
+$search_string = esc_attr(trim($search));
 
-$search_string = esc_attr( trim( get_query_var( 's' ) ) );
-$user_query = new WP_User_Query( array(
-      'search' => "*{$search}*",
-      'search_columns' => array(
-            'user_login',
-            'user_nicename',
-            'user_email',
-            'user_url',
-      ),
-      'meta_query' => array(
-            'relation' => 'OR',
-            array(
-                  'key' => 'first_name',
-                  'value' => $search,
-                  'compare' => 'LIKE'
-            ),
-            array(
-                  'key' => 'last_name',
-                  'value' => $search,
-                  'compare' => 'LIKE'
-            )
-      )
-          ) );
-$users_found = $user_query->get_results();
+$search_params = array(
+    'search' => "*{$search}*",
+    'search_columns' => array(
+        'user_login',
+        'user_nicename',
+        'user_email',
+        'user_url',
+    ));
+$user_query = new WP_User_Query($search_params);
+/* $users_found = $user_query->get_results(); */
 ?>
-<div class="co-1">
-      <section>
-            <header>
-                  <h2>
-                        Usuarios
-                  </h2>
-                  <hr class="cb pb5">
-            </header>
-            <?php
-            if ( !empty( $user_query->results ) ) {
-                  ?>
-                  <ul>
-                        <?php
-                        foreach ( $user_query->results as $user ) {
-                              ?>
-                              <li class="col-1-4 fl">
-                                    <article class="">
-                                          <a href="<?php echo get_author_posts_url( $user->ID ); ?>" class="fl">
-                                                <?php
-                                                $url = nz_get_user_image( $user->ID, 'profile' );
-                                                ?>
-                                                <img src="<?php echo $url ?>" alt="clubber-mag-profile-picture" width="100" height="100">
-                                          </a>
-                                          <div class="fl">
-                                                <a class="ml5" href="<?php echo get_author_posts_url( $user->ID ); ?>" >
-
-                                                      <?php echo $user->display_name ?>
-                                                </a>
-                                          </div>
-                                    </article>
-                              </li>
-                              <?php
-                        }
-                        ?>
-                  </ul>
-                  <?php
-            } else {
-                  ?>
-                  <p class="meddium">Ningún usuario encontrado con <span style="font-style:italic">'<?php echo get_query_var( 's' ) ?>'</span></p>
-                  <?php
-            }
+<div class="box-5">
+    <section>
+        <header>
+            <h2>
+                Usuarios
+            </h2>
+            <hr class="cb pb5">
+        </header>
+        <?php
+        if (!empty($user_query->results)) {
             ?>
-      </section>  
-</div>
-<div class="col-1 mt15">
-      <section>
-            <header>
-                  <h2>
-                        <?php _e( 'Other results', 'cm' ) ?>
-                  </h2>
-                  <hr class="cb pb5">
-            </header>
-            <?php
-            if ( have_posts() ) {
-                  ?>
-                  <ul class="archive-list">
-                        <?php
-                        while ( have_posts() ) {
-                              the_post();
-                              ?>
-                              <li>
-                                    <?php
-                                    get_template_part( 'tpl/parts/list-3' );
-                                    ?>
-                              </li>
+            <ul>
+                <?php
+                foreach ($user_query->results as $user) {
+                    ?>
+                    <li class="col-1-4 fl">
+                        <article class="">
+                            <a href="<?php echo get_author_posts_url($user->ID); ?>" class="fl">
+                                <?php
+                                $url = nz_get_user_image($user->ID, 'profile');
+                                ?>
+                                <img src="<?php echo $url ?>" alt="clubber-mag-profile-picture" width="100" height="100">
+                            </a>
+                            <div class="fl">
+                                <a class="ml5" href="<?php echo get_author_posts_url($user->ID); ?>" >
 
-                              <?php
-                        }
-                        ?>
-                  </ul>
-                  <?php
-            } else {
-                  ?>
-                  <p class="meddium"><?php _e( 'No Posts Found.', 'cm' ); ?> con <span style="font-style:italic">'<?php echo get_query_var( 's' ) ?>'</span></p>
-                  <?php
-            }
+                                    <?php echo $user->display_name ?>
+                                </a>
+                            </div>
+                        </article>
+                    </li>
+                    <?php
+                }
+                ?>
+            </ul>
+            <?php
+        } else {
             ?>
-      </section>
-      <?php
-      include (locate_template( 'tpl/parts/pagination.php' ));
-      ?>
+            <p class="meddium"><?php _e('No users found', 'cm'); ?> <?php _e('with', 'cm'); ?> <span style="font-style:italic">'<?php echo get_query_var('s') ?>'</span></p>
+            <?php
+        }
+        ?>
+    </section>  
+
+    <div class="group pb15"></div>
+    <section>
+        <header>
+            <h2>
+                <?php _e('Other results', 'cm') ?>
+            </h2>
+            <hr class="cb pb5">
+        </header>
+        <?php
+        if (have_posts()) {
+            ?>
+            <ul class="archive-list">
+                <?php
+                while (have_posts()) {
+                    the_post();
+                    ?>
+                    <li>
+                        <?php
+                        get_template_part('tpl/parts/list-3');
+                        ?>
+                    </li>
+                    <?php
+                }
+                ?>
+            </ul>
+            <?php
+        } else {
+            ?>
+            <p class="meddium"><?php _e('No content found', 'cm'); ?> <?php _e('with', 'cm'); ?> <span style="font-style:italic">'<?php echo get_query_var('s') ?>'</span></p>
+            <?php
+        }
+        ?>
+    </section>
+    <?php
+    include (locate_template('tpl/parts/pagination.php'));
+    ?>
 </div>
