d4c644a72fa689b44ebf5b01e52e08c32e97c0e4
NadirZenith <2cb.md2@gmail.com>
2015-04-17 00:56:21 +0200

    mobile slider

--- wp-content/themes/clubber-magazine/base.php
+++ wp-content/themes/clubber-magazine/base.php
@@ -1,49 +1,47 @@
 <?php ob_start() ?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
-      <?php get_template_part( 'tpl/base/head' ); ?>
-      <body <?php body_class(); ?> >
+    <?php get_template_part('tpl/base/head'); ?>
+    <body <?php body_class(); ?> >
 
-            <?php do_action( 'base_after_body' ) ?>
-            <div class="site">
-                  <header id="header" class="cb" role="banner">
-                        <?php get_template_part( 'tpl/base/header' ); //the header of page, logo banner menu etc...  ?>
-                  </header>
+        <?php do_action('base_after_body') ?>
+        <div class="site">
+            <header id="header" class="cb" role="banner">
+                <?php get_template_part('tpl/base/header'); //the header of page, logo banner menu etc...  ?>
+            </header>
 
-                  <?php
-                  if ( is_front_page() ) {
-                        ?>
-                <div class="oh">
-                        <section class="featured-posts">
-                              <?php
-                              echo do_shortcode( '[metaslider id=661]' );
-                              ?>
-                        </section>
-                </div>
-                        <?php
-                  } else if ( is_singular( 'artist' ) ) {
-                        echo get_template_part( 'tpl/parts/artist-home' );
-                  }
-                  ob_flush();
-                  ob_start();
-                  ?>
-                  <div id="content" class="container cb group">
-                        <?php nzs_display_messages(); ?>
-                        <?php get_template_part( 'tpl/base/content' ); ?>
-                  </div>
-                  <?php
-                  ob_flush();
-                  ob_start();
-                  ?>
-
-                  <footer id="footer" class="group pr" role="contentinfo">
-                        <?php get_template_part( 'tpl/base/footer' ); ?>
-                  </footer>
+            <?php
+            if (is_front_page()) {
+                ?>
+                <section class="featured-posts">
+                    <?php
+                    echo do_shortcode('[metaslider id=661]');
+                    ?>
+                </section>
+                <?php
+            } else if (is_singular('artist')) {
+                echo get_template_part('tpl/parts/artist-home');
+            }
+            ob_flush();
+            ob_start();
+            ?>
+            <div id="content" class="container cb group">
+                <?php nzs_display_messages(); ?>
+                <?php get_template_part('tpl/base/content'); ?>
             </div>
+            <?php
+            ob_flush();
+            ob_start();
+            ?>
 
-            <?php wp_footer(); ?>
+            <footer id="footer" class="group pr" role="contentinfo">
+                <?php get_template_part('tpl/base/footer'); ?>
+            </footer>
+        </div>
 
-      </body>
+        <?php wp_footer(); ?>
+
+    </body>
 </html>
 <?php
 ob_flush();
