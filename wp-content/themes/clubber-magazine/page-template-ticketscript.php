545d6ed27f64d6efb9168813cc098808edf90241
NadirZenith <2cb.md2@gmail.com>
2015-05-26 16:33:35 +0200

    ticketscript

--- wp-content/themes/clubber-magazine/page-template-ticketscript.php
+++ wp-content/themes/clubber-magazine/page-template-ticketscript.php
@@ -7,14 +7,20 @@
  */
 ?>
 
-<div class="col-1d col-sm-1-2" style="display: block; width: 600px;margin-left: auto;margin-right: auto">
+<div class="" style="display: block; width: 600px;margin-left: auto;margin-right: auto">
     <div class="ibox-5">
         <div class="box-5">
             <?php
-            if (wp_is_mobile()) {
-                NzWpCmTicketscript::get_mobile_iframe('LSSX45SZ');
+            if (class_exists('NzWpCmTicketscript')) {
+                if (wp_is_mobile()) {
+                    NzWpCmTicketscript::get_mobile_iframe('LSSX45SZ');
+                } else {
+                    NzWpCmTicketscript::get_web_iframe('LSSX45SZ');
+                }
             } else {
-                NzWpCmTicketscript::get_web_iframe('LSSX45SZ');
+                if (current_user_can('manage_options')) {
+                    echo '- activate ticketscript plugin -';
+                }
             }
             ?>
 
