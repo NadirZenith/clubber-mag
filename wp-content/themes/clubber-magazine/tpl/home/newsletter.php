1bdad7683441bf512bdc6df4b4019ba57332fbc2
NadirZenith <2cb.md2@gmail.com>
2015-07-09 19:10:27 +0200

    updated

--- wp-content/themes/clubber-magazine/tpl/home/newsletter.php
+++ wp-content/themes/clubber-magazine/tpl/home/newsletter.php
@@ -4,12 +4,12 @@
         cm_home_list_title('post', 'Newsletter');
         ?>
     </div>
-    <div class="newsletter-container tc p5 oh block-5" style="height: 100px;">
+    <div class="newsletter-container tc p5 oh block-5" style="height: 120px;">
         <form class="nzwpnewsletter">
             <div class="validation sc-3" style="background-color: rgba(255,255,255,0.5)"></div>
             <label for="email" class="sc-3 bold">
                 <?php _e('Insert your email to receive our newsletter', 'cm'); ?>
-                <input style="width:50%;max-width: 200px" class="col-1" type="email" id="newsletter-email2" name="email"/>
+                <input style="width:50%;max-width: 200px" class="col-1" type="email" id="newsletter-email" name="email"/>
             </label>
             <div class="cb mt15"></div>
             <input type="submit" class="readmore" value="<?php _e('Send', 'cm'); ?>"/>
