033ec4110a1fafd265a2e1e6918f4fa539659a1d
NadirZenith <2cb.md2@gmail.com>
2015-07-22 16:34:15 +0200

    update list order, added new archives

--- wp-content/themes/clubber-magazine/archive-music.php
+++ wp-content/themes/clubber-magazine/archive-music.php
@@ -1,117 +1,173 @@
 <section class="m5">
-      <header class="mt5 ml5 mb10">
-          <h1 class="sc-2">
-                  <?php _e( post_type_archive_title( '', false ), 'cm' ); ?>     
-            </h1>
-      </header>
+    <header class="mt5 ml5 mb10">
+        <h1 class="sc-2">
+            <?php _e(post_type_archive_title('', false), 'cm'); ?>     
+        </h1>
+    </header>
 
-      <ul>
-            <?php
-            $args = array(
-                  /* 'label_name' => 'Artistas', */
-                  'post_type' => 'artist',
-                  'post_status' => 'publish',
-                  'posts_per_page' => 1,
-                  'orderby' => 'rand',
-            );
-            $query = new WP_Query( $args );
-            if ( $query->have_posts() ) {
+    <ul>
+        <?php
+        //artists
+        $args = array(
+            'post_type' => 'artist',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
 
-                  while ( $query->have_posts() ) {
-                        $query->the_post();
-                        $item[ 'title' ] = '<span class="sc-1">' . __( 'Artists', 'cm' ) . '</span> ' . get_the_title();
-                        $item[ 'content' ] = wp_trim_words( get_the_content(), 20 );
-                        $item[ 'link' ] = get_post_type_archive_link( 'artist' );
-                        $item[ 'thumbnail' ] = get_the_post_thumbnail( null, '340-155-thumb' );
-                        ?>
-                        <li class="ibox-5 col-1">
-                              <?php
-                              include 'tpl/home/list-4.php';
-                              ?>
-                        </li>
-                        <?php
-                  }
+            while ($query->have_posts()) {
+                $query->the_post();
+                $item['title'] = '<span class="sc-1">' . __('Artists', 'cm') . '</span> ' . get_the_title();
+                $item['content'] = wp_trim_words(get_the_content(), 20);
+                $item['link'] = get_post_type_archive_link('artist');
+                $item['thumbnail'] = get_the_post_thumbnail(null, '340-155-thumb');
+                ?>
+                <li class="ibox-5 col-1">
+                    <?php
+                    include 'tpl/home/list-4.php';
+                    ?>
+                </li>
+                <?php
             }
+        }
 
-            wp_reset_postdata();
-            $args = array(
-                  'post_type' => 'podcast',
-                  'post_status' => 'publish',
-                  'posts_per_page' => 1,
-                  'orderby' => 'rand',
-                  'meta_query' => array(
-                        'relation' => 'AND',
-                        array(
-                              'key' => 'soundcloud_special_guest',
-                              'compare' => 'EXISTS',
-                        ),
-                        array(
-                              'key' => '_thumbnail_id',
-                              'compare' => 'EXISTS',
-                        )
-                  )
-            );
-            $query = new WP_Query( $args );
-            if ( $query->have_posts() ) {
+        wp_reset_postdata();
 
-                  while ( $query->have_posts() ) {
-                        $query->the_post();
-                        $item[ 'title' ] = '<span class="sc-1">' . __( 'Podcasts', 'cm' ) . '</span> ' . get_the_title();
-                        $item[ 'content' ] = wp_trim_words( get_the_content(), 20 );
-                        $item[ 'link' ] = get_post_type_archive_link( 'podcast' );
-                        $item[ 'thumbnail' ] = get_the_post_thumbnail( null, '340-155-thumb' );
-                        ?>
-                        <li class="ibox-5 col-1">
-                              <?php
-                              include 'tpl/home/list-4.php';
-                              ?>
-                        </li>
-                        <?php
-                  }
+         //into the beat
+        $args = array(
+            'post_type' => 'into-the-beat',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+            'meta_query' => array(
+                array(
+                    'key' => '_thumbnail_id',
+                    'compare' => 'EXISTS',
+                )
+            )
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
+
+            while ($query->have_posts()) {
+                $query->the_post();
+                $item['title'] = '<span class="sc-1">' . __('Into the Beat', 'cm') . '</span> ' . get_the_title();
+                $item['content'] = wp_trim_words(get_the_content(), 20);
+                $item['link'] = get_post_type_archive_link('into-the-beat');
+                $item['thumbnail'] = get_the_post_thumbnail(null, '340-155-thumb');
+                ?>
+                <li class="ibox-5 col-1">
+                    <?php
+                    include 'tpl/home/list-4.php';
+                    ?>
+                </li>
+                <?php
             }
+        }
 
-            wp_reset_postdata();
+        wp_reset_postdata();
+        
+        //open frequency
+        $args = array(
+            'post_type' => 'open-frequency',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
 
-            $post_type = 'music';
-            $taxonomy = 'music_type';
+            while ($query->have_posts()) {
+                $query->the_post();
+                $item['title'] = '<span class="sc-1">' . __('Open Frequency', 'cm') . '</span> ' . get_the_title();
+                $item['content'] = wp_trim_words(get_the_content(), 20);
+                $item['link'] = get_post_type_archive_link('open-frequency');
+                $item['thumbnail'] = get_the_post_thumbnail(null, '340-155-thumb');
+                $item['thumbnail'] = '<img src="' . get_site_url() . '/wp-content/themes/clubber-magazine/assets/images/types/clubber-mag-open-frequency.jpg"/>';
+                ?>
+                <li class="ibox-5 col-1">
+                    <?php
+                    include 'tpl/home/list-4.php';
+                    ?>
+                </li>
+                <?php
+            }
+        }
 
-            $music_terms = get_terms( $taxonomy, array(
-                  'orderby' => 'count',
-                  'hide_empty' => 0
-                      ) );
+        wp_reset_postdata();
+       
+        /*
+         */
+        //labels
+        $args = array(
+            'post_type' => 'label',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+        );
+        $query = new WP_Query($args);
+        if ($query->have_posts()) {
 
-            foreach ( $music_terms as $term ) {
-                  $term_link = get_term_link( $term );
-                  $args = array(
-                        'posts_per_page' => 1,
-                        'post_type' => $post_type,
-                        'orderby' => 'rand',
-                        'tax_query' => array(
-                              array(
-                                    'taxonomy' => 'music_type',
-                                    'field' => 'slug',
-                                    'terms' => $term->slug
-                              )
-                        )
-                  );
+            while ($query->have_posts()) {
+                $query->the_post();
+                $item['title'] = '<span class="sc-1">' . __('Labels', 'cm') . '</span> ' . get_the_title();
+                $item['content'] = wp_trim_words(get_the_content(), 20);
+                $item['link'] = get_post_type_archive_link('label');
+                $item['thumbnail'] = get_the_post_thumbnail(null, '340-155-thumb');
+                ?>
+                <li class="ibox-5 col-1">
+                    <?php
+                    include 'tpl/home/list-4.php';
+                    ?>
+                </li>
+                <?php
+            }
+        }
 
-                  $query = new WP_Query( $args );
-                  if ( $query->have_posts() ) {
-                        $query->the_post();
-                        $item[ 'title' ] = '<span class="sc-1">' . $term->name . '</span> ' . get_the_title();
-                        $item[ 'content' ] = wp_trim_words( get_the_content(), 20 );
-                        $item[ 'link' ] = $term_link;
-                        $item[ 'thumbnail' ] = get_the_post_thumbnail( null, '340-155-thumb' );
-                        ?>
-                        <li class="ibox-5 col-1">
-                              <?php
-                              include 'tpl/home/list-4.php';
-                              ?>
-                        </li>
-                        <?php
-                  }// have posts
-            }//for each
-            wp_reset_postdata();
-            ?>
-      </ul>
+        wp_reset_postdata();
+
+        //music items
+        $post_type = 'music';
+        $taxonomy = 'music_type';
+
+        $music_terms = get_terms($taxonomy, array(
+            'orderby' => 'count',
+            'hide_empty' => 0
+            ));
+        foreach ($music_terms as $term) {
+            $term_link = get_term_link($term);
+            $args = array(
+                'posts_per_page' => 1,
+                'post_type' => $post_type,
+                'orderby' => 'rand',
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'music_type',
+                        'field' => 'slug',
+                        'terms' => $term->slug
+                    )
+                )
+            );
+
+            $query = new WP_Query($args);
+            if ($query->have_posts()) {
+                $query->the_post();
+                $item['title'] = '<span class="sc-1">' . $term->name . '</span> ' . get_the_title();
+                $item['content'] = wp_trim_words(get_the_content(), 20);
+                $item['link'] = $term_link;
+                $item['thumbnail'] = get_the_post_thumbnail(null, '340-155-thumb');
+                ?>
+                <li class="ibox-5 col-1">
+                    <?php
+                    include 'tpl/home/list-4.php';
+                    ?>
+                </li>
+                <?php
+            }// have posts
+        }//for each
+        wp_reset_postdata();
+        ?>
+    </ul>
 </section>
