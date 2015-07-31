410b8a18fc05f26c4183ed6105b139e37aebec78
NadirZenith <2cb.md2@gmail.com>
2015-07-31 15:51:20 +0200

    update

--- wp-content/themes/clubber-magazine/tpl/home/newsletter.php
+++ wp-content/themes/clubber-magazine/tpl/home/newsletter.php
@@ -4,17 +4,27 @@
         cm_home_list_title('post', 'Newsletter');
         ?>
     </div>
-    <div class="newsletter-container tc p5 oh block-5" style="height: 120px;">
-        <form class="nzwpnewsletter">
-            <div class="validation sc-3" style="background-color: rgba(255,255,255,0.5)"></div>
-            <label for="email" class="sc-3 bold">
-                <?php _e('Insert your email to receive our newsletter', 'cm'); ?>
-                <input style="width:50%;max-width: 200px" class="col-1" type="email" id="newsletter-email" name="email"/>
-            </label>
-            <div class="cb mt15"></div>
-            <input type="submit" class="readmore" value="<?php _e('Send', 'cm'); ?>"/>
-        </form>
+    <div class="newsletter-container tc p5 oh block-5" style="">
 
+        <div class="nzwpnewsletter">
+            <div class="validation sc-3"></div>
+
+            <form class="subscribe">
+                <label for="subscribe_email">
+                    <?php _e('Insert your email to receive our newsletter', 'cm'); ?>
+                </label><br>
+                <input type="text" name="subscribe_email"/><br>
+                <input type="submit" value="Subscribe"/>
+                <a class="switch"><?php _e('unsubscribe', 'nzwpnewsletter') ?></a>
+            </form>
+            <form class="unsubscribe" style="display: none">
+                <label for="unsubscribe_email">
+                    <?php _e('Unsubscribe from newsletter', 'nzwpnewsletter') ?>
+                </label><br>
+                <input type="text" name="unsubscribe_email"/><br>
+                <input type="submit" value="Unsubscribe"/>
+                <a class="switch"><?php _e('subscribe', 'nzwpnewsletter') ?></a>
+            </form>
+        </div>
     </div>
 </div>
-
