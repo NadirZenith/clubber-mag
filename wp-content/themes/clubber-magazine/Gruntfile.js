f926fca105aade732003b7000465873b9ed1d028
NadirZenith <2cb.md2@gmail.com>
2015-08-17 16:25:00 +0200

    grunt build

--- wp-content/themes/clubber-magazine/Gruntfile.js
+++ wp-content/themes/clubber-magazine/Gruntfile.js
@@ -107,7 +107,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: {
-                    'lib/scripts.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
+                    'lib/assets.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
                 }
             }
         },
