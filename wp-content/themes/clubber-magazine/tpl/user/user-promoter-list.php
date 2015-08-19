de442ab6cf4e8d75c4251589201fe0709933e7c1
NadirZenith <2cb.md2@gmail.com>
2015-08-19 14:59:25 +0200

    updates..

--- wp-content/themes/clubber-magazine/tpl/user/user-promoter-list.php
+++ wp-content/themes/clubber-magazine/tpl/user/user-promoter-list.php
@@ -23,17 +23,14 @@ $args = array(
 
 $wp_query = new WP_Query($args);
 ?>
-<div id="primary">
-    <section class="pb15">
-        <div class="ml5 cb group">
-            <?php
-            $user_agenda_url = get_author_posts_url($curauth->ID) . 'agenda';
-            ?>
-            <h1 class="fl"><span title="<?php _e('User events') ?>"><?php _e('User events'); ?></span></h1>
-            <span class="fr mr5 mt5">
-                [ <a href="<?php echo get_permalink(get_page_by_path('subir-evento')) ?>"><?php _e('Share event', 'cm') ?></a> ]
-            </span>
-        </div>
+<main role="main" class="has-sidebar">
+    <section>
+        <header>
+            <h1>
+                <?php _e('User events'); ?>
+            </h1>
+        </header>
+
 
         <?php
         if (have_posts()) {
@@ -43,35 +40,14 @@ $wp_query = new WP_Query($args);
                 while (have_posts()) {
                     the_post();
                     ?>
-                    <li class="col-1-4 fl">
-                        <article>
-                            <h2 class="hover" style="line-height: normal;" >
-                                <a href="<?php the_permalink(); ?>" style="">
-                                    <?php
-                                    echo get_the_title();
-                                    ?>
-                                </a>
-                            </h2>
-                            <div class="p-detail">
-                                <?php
-                                $date = get_post_meta(get_the_ID(), 'wpcf-event_begin_date', true);
-                                echo date('d/m/y', $date);
-                                $tax = 'city';
-                                if ($term = wp_get_post_terms(get_the_ID(), $tax)[0]->name) {
-                                    $link = get_term_link($term, $tax);
-                                    echo " <a href='{$link}'>{$term}</a>";
-                                }
-                                ?>
-                            </div>
-                            <a class="featured-image" href="<?php echo get_permalink($event->ID); ?>">
-                                <?php echo get_the_post_thumbnail(get_the_ID(), '290-160-thumb'); ?>
-                            </a>
-                        </article>
-                    </li>    
+                    <li class="pure-u-1-2 pure-u-md-1-5">
+                        <div class="p3">
+                            <?php get_template_part('tpl/list/list-2'); ?>
+                        </div>
+                    </li>   
                     <?php
                 }
                 ?>
-
             </ul>
             <?php
         } else {
@@ -89,8 +65,7 @@ $wp_query = new WP_Query($args);
         }
         ?>
     </section>
-
-</div>
-<div id="secondary">
-    <?php get_sidebar('right'); ?>
-</div>
+</main>
+<aside class="<?php echo roots_sidebar_class(); ?>" role="complementary">
+    <?php include roots_sidebar_path(); ?>
+</aside>
