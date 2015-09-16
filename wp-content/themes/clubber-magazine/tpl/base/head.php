b67aa57c405bc36a62af866ed4800c5637c17ecf
NadirZenith <2cb.md2@gmail.com>
2015-09-16 18:16:00 +0200

    ..

--- wp-content/themes/clubber-magazine/tpl/base/head.php
+++ wp-content/themes/clubber-magazine/tpl/base/head.php
@@ -1,6 +1,7 @@
 <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="viewport" content="user-scalable=0;"/>
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title><?php wp_title( '|', true, 'right' ); ?></title>
       <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon.png" />
